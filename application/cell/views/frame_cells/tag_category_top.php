9865ac78fe30f38c3a50eee3a87fabc5cad1055e
oa <comdan66@gmail.com>
2014-08-10 23:48:08 +0800

    初步調整成 cell 方式。

---
+++ application/cell/views/frame_cells/tag_category_top.php
@@ -0,0 +1,18 @@
+
+<div id='tag_category_top'>
+  <a href=''>傳統小吃</a>
+  |
+  <a href=''>早午餐</a>
+  |
+  <a href=''>老街美食</a>
+  |
+  <a href=''>隱藏版</a>
+  |
+  <a href=''>旅遊必吃</a>
+  |
+  <a href=''>下午茶</a>
+  |
+  <a href=''>深夜美食</a>
+  |
+  <a href=''>在地特色</a>
+</div>
---
+++ application/cell/views/main_cells/promos.php
@@ -0,0 +1,50 @@
+
+<div id='promos' class='row'>
+  <div class='col-md-5' id='promo1'>
+    <a href=''>
+      <img src='http://photo.hanyu.iciba.com/upload/encyclopedia_2/2c/b2/bk_2cb2a429d1215ed56fa9bd292a94d0ea_WYned6.jpg'>
+      <div class='promo1_text'>
+        <div class='promo1_text_box'>
+          <h4>滷肉飯</h4>
+          滷肉飯是台灣流行的一道豬肉飯菜餚。近年台灣官方有意將滷肉飯英譯，改為台語發音的直譯 一般常見魯肉飯的「魯」，其實...
+        </div>
+      </div>
+    </a>
+  </div>
+  <div class='col-md-4' id='promo2-5'>
+    <div class='row'>
+      <div class='col-md-6' id='promo2'>
+        <img src='http://pic.pimg.tw/yingoyingo/1217952449.jpg'>
+        <div class='promo2-5_text'>
+          北港 紅燒青蛙
+        </div>
+      </div>
+      <div class='col-md-6' id='promo3'>
+        <img src='http://0492353722.tranews.com/images/Info/Y014147000001_6_1.jpg'>
+        <div class='promo2-5_text'>
+          基隆 廟口鼎邊銼
+        </div>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col-md-6' id='promo4'>
+        <img src='http://ytc920.files.wordpress.com/2008/08/r0021246.jpg'>
+        <div class='promo2-5_text'>
+          淡水 阿給
+        </div>
+      </div>
+      <div class='col-md-6' id='promo5'>
+        <img src='http://youtiao.ejinqiao.com/UploadFile/Product/20129/201209031214199042s.jpg'>
+        <div class='promo2-5_text'>
+          經典傳統 豆漿油條
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='col-md-3' id='promo6'>
+    <img src='http://ext.pimg.tw/chrisya8/1387978324-2397591363_n.jpg'>
+    <div class='promo6_text'>
+      民國前三年蔡家曾祖父為養育五子、六女，以扛擔方式販售白煮鴨及燻鴨維生，期間子女陸續長大成人...
+    </div>
+  </div>
+</div>
