a46c9d5aefe46a15a22a53c046b284338b181ef3
oa <comdan66@gmail.com>
2014-08-11 08:53:14 +0800

    Fix  controller extends bug.

--- application/controllers/delay/main.php
+++ application/controllers/delay/main.php
@@ -4,7 +4,7 @@
  * @author      OA Wu <comdan66@gmail.com>
  * @copyright   Copyright (c) 2014 OA Wu Design
  */
-class Main extends Admin_controller {
+class Main extends Delay_controller {
   public function __construct () {
     parent::__construct ();
   }
